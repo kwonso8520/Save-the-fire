04800c9bee0176ec37f40034dd95ab84588e1c6a
kwonso8520 <rnjstjrdnjs@dgsw.hs.kr>
2024-03-29 21:46:10 +0900

    .

--- Assets/Scripts/Enemy/EnemySpawner.cs
+++ Assets/Scripts/Enemy/EnemySpawner.cs
@@ -6,8 +6,6 @@ using static UnityEngine.GraphicsBuffer;
 public class EnemySpawner : MonoBehaviour
 {
     [SerializeField]
-    float _respawnTime;
-    [SerializeField]
     float _enemySpeed = 5f;
     private Transform _target;
 
@@ -32,10 +30,25 @@ public class EnemySpawner : MonoBehaviour
             float x = Mathf.Cos(ran * Mathf.Deg2Rad) * 4.3f; // 정해진 위치에서 5만큼 떨어진 원형 랜덤 방향으로 생성
             float y = Mathf.Sin(ran * Mathf.Deg2Rad) * 4.3f; // 정해진 위치에서 5만큼 떨어진 원형 랜덤 방향으로 생성
             Vector3 pos = transform.position + new Vector3(x, y, 0);
-            var EnemyObj = EnemyPool.GetObject();
-            EnemyObj.GetComponent<Transform>().position = pos;
-            EnemyObj.GetComponent<Rigidbody2D>().AddForce((_target.position - EnemyObj.transform.position).normalized * _enemySpeed, ForceMode2D.Impulse);
-            yield return new WaitForSeconds(Random.Range(0.5f, 1.5f));
+            int patternNum = Random.Range(0, 2);
+            switch (patternNum)
+            {
+                case 0:
+                    var EnemyObj = EnemyPool.GetObject();
+                    EnemyObj.GetComponent<Transform>().position = pos;
+                    EnemyObj.GetComponent<Rigidbody2D>().AddForce((_target.position - EnemyObj.transform.position).normalized * _enemySpeed, ForceMode2D.Impulse);
+                    yield return new WaitForSeconds(Random.Range(0.5f, 1.5f));
+                    break;
+                case 1:
+                    for (int i = 0; i < 5; i++)
+                    {
+                        EnemyObj = EnemyPool.GetObject();
+                        EnemyObj.GetComponent<Transform>().position = pos;
+                        EnemyObj.GetComponent<Rigidbody2D>().AddForce((_target.position - EnemyObj.transform.position).normalized * _enemySpeed, ForceMode2D.Impulse);
+                    }
+                    break;
+            }
+            
         }
     }
 }
