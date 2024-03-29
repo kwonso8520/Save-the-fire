079529551d48fac95da43e1ed929b2df2bcca28e
kwonso8520 <rnjstjrdnjs@dgsw.hs.kr>
2024-03-29 23:22:12 +0900

    patternAdd
    패턴3개추가

--- Assets/Scripts/Enemy/EnemySpawner.cs
+++ Assets/Scripts/Enemy/EnemySpawner.cs
@@ -1,7 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using static UnityEngine.GraphicsBuffer;
 
 public class EnemySpawner : MonoBehaviour
 {
@@ -18,37 +17,64 @@ public class EnemySpawner : MonoBehaviour
         _target = FindFirstObjectByType<PlayerController>().transform;
     }
 
-    void Update()
-    {
-       
-    }
     IEnumerator SpawnEnemy()
     {
         while (true)
         {
-            int ran = Random.Range(0, 360); //랜덤으로 0~360도
-            float x = Mathf.Cos(ran * Mathf.Deg2Rad) * 4.3f; // 정해진 위치에서 5만큼 떨어진 원형 랜덤 방향으로 생성
-            float y = Mathf.Sin(ran * Mathf.Deg2Rad) * 4.3f; // 정해진 위치에서 5만큼 떨어진 원형 랜덤 방향으로 생성
-            Vector3 pos = transform.position + new Vector3(x, y, 0);
-            int patternNum = Random.Range(0, 2);
+            int patternNum = Random.Range(0, 3);
             switch (patternNum)
             {
                 case 0:
-                    var EnemyObj = EnemyPool.GetObject();
-                    EnemyObj.GetComponent<Transform>().position = pos;
-                    EnemyObj.GetComponent<Rigidbody2D>().AddForce((_target.position - EnemyObj.transform.position).normalized * _enemySpeed, ForceMode2D.Impulse);
-                    yield return new WaitForSeconds(Random.Range(0.5f, 1.5f));
+                    StartCoroutine(PatternOne());
                     break;
                 case 1:
-                    for (int i = 0; i < 5; i++)
-                    {
-                        EnemyObj = EnemyPool.GetObject();
-                        EnemyObj.GetComponent<Transform>().position = pos;
-                        EnemyObj.GetComponent<Rigidbody2D>().AddForce((_target.position - EnemyObj.transform.position).normalized * _enemySpeed, ForceMode2D.Impulse);
-                    }
+                    StartCoroutine(PatternTwo());
+                    break;
+                case 2:
+                    StartCoroutine(PatternThree());
                     break;
             }
             
         }
+    }
+    IEnumerator PatternOne()
+    {
+        int ran = Random.Range(0, 360); //랜덤으로 0~360도
+        float x = Mathf.Cos(ran * Mathf.Deg2Rad) * 4.3f; // 정해진 위치에서 5만큼 떨어진 원형 랜덤 방향으로 생성
+        float y = Mathf.Sin(ran * Mathf.Deg2Rad) * 4.3f; // 정해진 위치에서 5만큼 떨어진 원형 랜덤 방향으로 생성
+        Vector3 pos = transform.position + new Vector3(x, y, 0);
+
+        var EnemyObj = EnemyPool.GetObject();
+        EnemyObj.GetComponent<Transform>().position = pos;
+        EnemyObj.GetComponent<Rigidbody2D>().AddForce((_target.position - EnemyObj.transform.position).normalized * _enemySpeed, ForceMode2D.Impulse);
+        yield return new WaitForSeconds(Random.Range(0.5f, 1.5f));
+    }
+    IEnumerator PatternTwo()
+    {
+        int ran = Random.Range(0, 360); //랜덤으로 0~360도
+        float x = Mathf.Cos(ran * Mathf.Deg2Rad) * 4.3f; // 정해진 위치에서 5만큼 떨어진 원형 랜덤 방향으로 생성
+        float y = Mathf.Sin(ran * Mathf.Deg2Rad) * 4.3f; // 정해진 위치에서 5만큼 떨어진 원형 랜덤 방향으로 생성
+        Vector3 pos = transform.position + new Vector3(x, y, 0);
+
+        for (int i = 0; i < 5; i++)
+        {
+            var EnemyObj = EnemyPool.GetObject();
+            EnemyObj.GetComponent<Transform>().position = pos;
+            EnemyObj.GetComponent<Rigidbody2D>().AddForce((_target.position - EnemyObj.transform.position).normalized * _enemySpeed, ForceMode2D.Impulse);
+            yield return new WaitForSeconds(0.5f);
+        }
+    }
+    IEnumerator PatternThree()
+    {
+        for (int i = 1; i <= 8; i++)
+        {
+            float x = Mathf.Cos((360 / i) * Mathf.Deg2Rad) * 4.3f; // 정해진 위치에서 5만큼 떨어진 원형 랜덤 방향으로 생성
+            float y = Mathf.Sin((360 / i) * Mathf.Deg2Rad) * 4.3f; // 정해진 위치에서 5만큼 떨어진 원형 랜덤 방향으로 생성
+            Vector3 pos = transform.position + new Vector3(x, y, 0);
+            var EnemyObj = EnemyPool.GetObject();
+            EnemyObj.GetComponent<Transform>().position = pos;
+            EnemyObj.GetComponent<Rigidbody2D>().AddForce((_target.position - EnemyObj.transform.position).normalized * _enemySpeed, ForceMode2D.Impulse);
+        }
+        yield return null;
     }
 }
